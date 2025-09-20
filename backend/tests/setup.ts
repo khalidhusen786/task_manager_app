1547c5bba217918e43ee788da5ad82e9772d05e5
Khalid Husen <khalidhusen000@gmail.com>
2025-09-21 00:40:31 +0300

    added modified setup for the test

--- backend/tests/setup.ts
+++ backend/tests/setup.ts
@@ -1,8 +1,8 @@
 // Test setup and configuration
-import dotenv from "dotenv";
-import path from "path";
-// Load .env.test instead of default .env
-dotenv.config({ path: path.resolve(__dirname, "../.env.test") });
+process.env.MONGODB_URI = "mongodb+srv://khalidhusen000_db_user:ywTPkQSZqylD0eLY@cluster0.nhgcnah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+process.env.COOKIE_SECRET = "15d9cb45c27d7d346f4af8df181ba581291abc3f70ac5e4a2119d5ae187a99ffae687a43955c39985a2f3361881648cb18681c5a38b35e0c205d147c13f3e3dc";
+process.env.JWT_SECRET = "f3b9c2a1d5e7f8a1234b6c7d8e9f0a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8";
+process.env.JWT_REFRESH_SECRET = "a8b7c6d5e4f3a2b1c0d9e8f7a6b5c4d3e2f1a0b9c8d7e6f5a4b3c2d1e0f9a8b7"
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { config } from '../src/config';
